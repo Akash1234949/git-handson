54f13f2fd444ad4bbec4c44581383697ab956acb
Akash1234949 <your.name@ibm.com>
2026-09-21 23:16:56 +0530

    Add activity_1 SQL file

---
+++ activity_1.sql
@@ -0,0 +1,10 @@
+ 
+DROP TABLE IF EXISTS salesman ;
+CREATE TABLE salesman (
+    salesman_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50),
+    commission DECIMAL(5, 2)
+);
+
+SELECT * FROM salesman;
